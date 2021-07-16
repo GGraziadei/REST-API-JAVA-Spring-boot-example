7c61c7224bc3902b518972b67ac574317f0d3d17
GGraziadei <graziadei.gianluca@gmail.com>
2021-07-16 03:08:41 +0200

    autenticazione

--- ProductAPI/ProductAPI/src/main/java/it/goodgamegroup/product/rest/controllers/ProductRestController.java
+++ ProductAPI/ProductAPI/src/main/java/it/goodgamegroup/product/rest/controllers/ProductRestController.java
@@ -2,6 +2,8 @@ package it.goodgamegroup.product.rest.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,13 +19,17 @@ public class ProductRestController {
 	@Autowired
 	ProductRepository repository;
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProductRestController.class);
+	
 	@RequestMapping(value = "/products", method = RequestMethod.GET)
 	public List<Product> getProducts() {
+		LOGGER.info("Request Product LIST");
 		return this.repository.findAll();
 	}
 	
 	@RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
 	public Product getProduct(@PathVariable("id") int id) {
+		LOGGER.info("Finding Product by ID " + id);
 		if(this.repository.findById(id).isEmpty() == false)
 			return this.repository.findById(id).get();
 		Product p = new Product();
--- ProductAPI/ProductAPI/src/test/java/it/goodgamegroup/product/rest/ProductApiApplicationTests.java
+++ ProductAPI/ProductAPI/src/test/java/it/goodgamegroup/product/rest/ProductApiApplicationTests.java
@@ -3,6 +3,7 @@ package it.goodgamegroup.product.rest;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,10 +12,13 @@ import it.goodgamegroup.product.rest.entities.Product;
 @SpringBootTest
 class ProductApiApplicationTests {
 
+	@Value("${productserviceapi.services.url}")
+	private String baseURL;
+	
 	@Test
 	void testGetProduct() {
 		RestTemplate restTemplate = new RestTemplate();
-		Product p = restTemplate.getForObject("http://localhost:8081/api/products/2", Product.class);
+		Product p = restTemplate.getForObject(baseURL+"/2", Product.class);
 		assertNotNull(p);
 		assertEquals("iPhoneXX", p.getName());
 	}
@@ -26,7 +30,7 @@ class ProductApiApplicationTests {
 		p.setName("Nome");
 		p.setDescription("DescrizioneTest");
 		p.setPrice(0);
-		Product p1 = restTemplate.postForObject("http://localhost:8081/api/products/",p, Product.class);
+		Product p1 = restTemplate.postForObject(baseURL,p,Product.class);
 		assertNotNull(p1);
 		assertEquals("Nome", p1.getName());
 	}
@@ -34,8 +38,8 @@ class ProductApiApplicationTests {
 	@Test
 	void testUpdateProduct() {
 		RestTemplate restTemplate = new RestTemplate();
-		Product p = restTemplate.getForObject("http://localhost:8081/api/products/2", Product.class);
+		Product p = restTemplate.getForObject(baseURL+"/2",Product.class);
 		p.setPrice(1);
-		restTemplate.put("http://localhost:8081/api/products/", p);
+		restTemplate.put(baseURL, p);
 	}
 }
